ff1a3f1a073f80d9474bf6b6b787cb2297169e41
Bỉnh Quyền <binhxquyen@BQs-Mac.local>
2023-06-19 22:00:00 +0700

    add: filter common

--- src/components/common/Header/common.constant.ts
+++ src/components/common/Header/common.constant.ts
@@ -1,22 +1,42 @@
 export const SORT_DATA = [
   {
-    label: "Chị em gái",
+    label: "Người yêu",
     value: "user_group1",
   },
   {
-    label: "Nữ giới",
+    label: "Vợ chồng",
     value: "user_group2",
   },
   {
-    label: "Vợ",
+    label: "Bố mẹ",
     value: "user_group3",
   },
   {
-    label: "Bạn gái",
+    label: "Anh em",
+    value: "user_group9",
+  },
+  {
+    label: "Chị em",
+    value: "user_group10",
+  },
+  {
+    label: "Bạn bè",
     value: "user_group4",
   },
   {
-    label: "Người yêu",
+    label: "Trẻ em",
     value: "user_group5",
+  },
+  {
+    label: "Đồng nghiệp",
+    value: "user_group6",
+  },
+  {
+    label: "Sếp",
+    value: "user_group7",
+  },
+  {
+    label: "Giáo viên",
+    value: "user_group8",
   },
 ];
--- src/pages/SortPage/common.ts
+++ src/pages/SortPage/common.ts
@@ -1,22 +1,42 @@
 export const SORT_ARRAY = [
   {
     name: "user_group1",
-    value: ["Nữ", "Đồng Nghiệp"],
+    value: ["Anh yêu", "Em yêu"],
   },
   {
     name: "user_group2",
-    value: ["Nữ"],
+    value: ["Vợ", "Chồng"],
   },
   {
     name: "user_group3",
-    value: ["Thầy cô"],
-  },
-  {
-    name: "user_group4",
-    value: ["Bố mẹ"],
+    value: ["Bố", "Mẹ"],
   },
   {
     name: "user_group5",
-    value: ["Trẻ em"],
+    value: ["Bé trai", "Bé gái"],
+  },
+  {
+    name: "user_group6",
+    value: ["Đồng nghiệp"],
+  },
+  {
+    name: "user_group7",
+    value: ["Sếp nam", "Sếp nữ"],
+  },
+  {
+    name: "user_group8",
+    value: ["Thầy", "Cô"],
+  },
+  {
+    name: "user_group4",
+    value: ["Bạn trai", "Bạn gái"],
+  },
+  {
+    name: "user_group9",
+    value: ["Anh trai", "Em trai"],
+  },
+  {
+    name: "user_group10",
+    value: ["Chị gái", "Em gái"],
   },
 ];
